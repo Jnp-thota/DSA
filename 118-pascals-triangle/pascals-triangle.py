0c3172ca9bb5191657e7fc26bdfe25d65b771a0e
Jaya Naga Prakash Thota <143789784+Jnp-thota@users.noreply.github.com>
2025-09-06 13:17:22 -0700

    Time: 35 ms (0.77%) | Memory: 17.9 MB (16.22%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,26 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+        for i in range(numRows):
+            dupList = []
+            if len(res)<=1:
+                for k in range(len(res)+1):
+                    dupList.append(1)
+                res.append(dupList)
+            else:
+                recLst = res[len(res)-1]
+                for j in range(len(recLst)+1):
+                    if j==0 or j==(len(recLst)):
+                        dupList.append(1)
+                    else:
+                        print(res,recLst,j)
+                        dupList.append(recLst[j-1]+recLst[j])
+                res.append(dupList)
+        return res
+                    
+
+
+            # for j in range(i+1):
+            #     if j ==0 or i==j:
+            #         dupList.add(1)
+            #     elif 
