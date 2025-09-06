42bdadcfdfc977ece9980d6adaa8d8e267c2c065
Jaya Naga Prakash Thota <143789784+Jnp-thota@users.noreply.github.com>
2025-09-06 13:38:11 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.00%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        res = []
+        for i in range(rowIndex+1):
+            dupList = []
+            if len(res)<=1:
+                for k in range(len(res)+1):
+                    dupList.append(1)
+                res.append(dupList)
+            else:
+                recLst = res[len(res)-1]
+                for j in range(len(recLst)+1):
+                    if j==0 or j==(len(recLst)):
+                        dupList.append(1)
+                    else:
+                        dupList.append(recLst[j-1]+recLst[j])
+                res.append(dupList)
+        print(len(res))
+        return res[rowIndex]
+        
